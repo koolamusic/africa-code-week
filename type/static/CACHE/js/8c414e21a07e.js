02e2896de02e967fa83acfd060c4f3a66f18efb0
Andrew <me@andrewmiracle.com>
2018-10-18 11:29:06 +0100

    create section for typing practice

---
+++ type/static/CACHE/js/8c414e21a07e.js
@@ -0,0 +1,6 @@
+;if(window.location.pathname==='/lessons/')
+{var menutab=document.getElementById("lessons");var att=document.createAttribute("class");att.value="active";menutab.setAttributeNode(att);}
+if(window.location.pathname==='/tutor/')
+{var menutab=document.getElementById("tutor");var att=document.createAttribute("class");att.value="active";menutab.setAttributeNode(att);}
+else if(window.location.pathname==='/customcode/')
+{var menutab=document.getElementById("customcode");var att=document.createAttribute("class");att.value="active";menutab.setAttributeNode(att);}
